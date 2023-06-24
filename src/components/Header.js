3ffc84f7598064dbf8ce9d524aeb4c30a32fec9e
Vibhav <vibhavsachan743@gmail.com>
2023-06-24 23:47:26 +0530

    Blog webpage completed with Api call

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='w-full flex justify-center items-center border-2 shadow-md py-2 fixed top-0 bg-white hover:shadow-lg'>
+      <header className='text-center' >
+        <h1 className='text-3xl font-bold uppercase'>My Blogs</h1>
+      </header>
+    </div>
+    )
+}
+
+export default Header
